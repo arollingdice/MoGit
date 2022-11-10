491206f0c436359162bed5d02b976ab406900a03
arollingdice <arollingdice@gmail.com>
2022-11-09 21:12:13 -0700

    debugging.

--- app/main.py
+++ app/main.py
@@ -80,11 +80,11 @@ def ls_tree(sha_1):
 
         else:
             break
-    print(hashed) 
     while(origin.find(n) != (len(origin) - 1)):
         s_idx = origin.find(s)
         n_idx = origin.find(n)
         #print(file_name)
+        print(origin)
         file_name = origin[s_idx + 1: n_idx].decode('utf-8')
         if file_name != "":
             reg_file.append(file_name)
