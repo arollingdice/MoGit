e6204edfd2b44d07b2b7248d667495e5432605c8
arollingdice <arollingdice@gmail.com>
2023-03-09 13:46:26 -0700

    added file type at the beginning of the objects, indicating if it is s 'blob' or something else. Now hashed content is: filetype + b'\x00' + actual_content.

--- ugit/data.py
+++ ugit/data.py
@@ -4,20 +4,29 @@ from pathlib import Path
 
 GIT_DIR = Path('.ugit')
 OBJECTS_DIR = GIT_DIR / "objects"
+NULL = b'\x00'
 
 def init():
     os.makedirs(GIT_DIR)
     os.makedirs(OBJECTS_DIR)
 
-def hash_object(data):
-    oid = hashlib.sha1(data).hexdigest()
-    GIT_DIR = Path(".ugit")
+def hash_object(data, type_='blob'):
+    obj = type_.encode() + NULL + data
+    oid = hashlib.sha1(obj).hexdigest()
 
     with open (OBJECTS_DIR / oid, 'wb') as out:
-        out.write(data)
+        out.write(obj)
 
     return oid
 
-def get_object(oid):
+def get_object(oid, expected='blob'):
     with open (OBJECTS_DIR / oid, 'rb') as f:
-        return f.read()
+        obj = f.read()
+    
+    type_, _, content = obj.partition(NULL)
+    type_ = type_.decode()
+
+    if expected is not None and type_ != expected:
+        raise ValuesError(f'Expected {expected}, got {type_}')
+
+    return content
