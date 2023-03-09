dcc656045a90bcfd69ff8c941d5abe5aaa06c9a0
arollingdice <arollingdice@gmail.com>
2023-03-09 12:06:56 -0700

    implemented 'ugit init': created a '.ugit' directory for ugit to store all the data.

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+
+from . import data
 
 def main():
     args = parse_args()
@@ -16,4 +19,6 @@ def parse_args():
     return parser.parse_args()
 
 def init(args):
-    print('Hello, World!')
+    data.init()
+    print('Initialized empy ugit repository in %s' 
+    % os.path.join(os.getcwd(), data.GIT_DIR))
