9d82f3b49a32a3d4a9491250db27206a7e16eda5
arollingdice <arollingdice@gmail.com>
2023-03-09 14:52:35 -0700

    finished write trees

--- ugit/base.py
+++ ugit/base.py
@@ -1,15 +1,28 @@
 import os
 from pathlib import Path
-
 from . import data
 
 
 def write_tree(directory='.'):
+    entries = []
     for entry in Path(directory).iterdir():
+        if is_ignored(entry):
+            continue
         if entry.is_file() and not entry.is_symlink():
-            # TODO write the file to object store
-            print(entry)
+            type_ = 'blob'
+            with open(entry, 'rb') as f:
+                oid = data.hash_object(f.read())
         elif entry.is_dir() and not entry.is_symlink():
-            write_tree(entry)
-    
-    # TODO actually create the tree object
+            type_ = 'tree'
+            oid = write_tree(entry)
+        entries.append((entry.name, oid, type_)) 
+
+    tree = ''.join(f'{type_} {oid} {name}\n'
+                    for name, oid, type_
+                    in sorted(entries))
+
+    return data.hash_object(tree.encode(), 'tree')
+                
+
+def is_ignored(path):
+    return '.ugit' in Path(path).parts
