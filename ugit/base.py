5c0eebc3b98f2f255141b356b278e61b8950a46a
arollingdice <arollingdice@gmail.com>
2023-03-09 17:55:58 -0700

    Documented the Tree objects related codes in base.py

--- ugit/base.py
+++ ugit/base.py
@@ -2,27 +2,94 @@ import os
 from pathlib import Path
 from . import data
 
-
-def write_tree(directory='.'):
+def write_tree (directory='.'):
     entries = []
-    for entry in Path(directory).iterdir():
-        if is_ignored(entry):
-            continue
-        if entry.is_file() and not entry.is_symlink():
-            type_ = 'blob'
-            with open(entry, 'rb') as f:
-                oid = data.hash_object(f.read())
-        elif entry.is_dir() and not entry.is_symlink():
-            type_ = 'tree'
-            oid = write_tree(entry)
-        entries.append((entry.name, oid, type_)) 
+    with os.scandir (directory) as it:
+        for entry in it:
+            full = f'{directory}/{entry.name}'
+            if is_ignored (full):
+                continue
+            if entry.is_file (follow_symlinks=False):
+                type_ = 'blob'
+                with open (full, 'rb') as f:
+                    oid = data.hash_object (f.read ())
+            elif entry.is_dir (follow_symlinks=False):
+                type_ = 'tree'
+                oid = write_tree (full)
+            entries.append ((entry.name, oid, type_))
 
-    tree = ''.join(f'{type_} {oid} {name}\n'
+    tree = ''.join (f'{type_} {oid} {name}\n'
                     for name, oid, type_
-                    in sorted(entries))
-
-    return data.hash_object(tree.encode(), 'tree')
+                    in sorted (entries))
+                    
+    return data.hash_object (tree.encode (), 'tree')
                 
 
+def _iter_tree_entries(oid):
+    """
+    to iterate over the entries in a Git tree object and 
+    generate a sequence of tuples representing those entries
+    """
+    if not oid:
+        return
+    tree = data.get_object(oid, 'tree')
+    for entry in tree.decode().splitlines():
+        type_, oid, name = entry.split(' ', 2)
+        yield type_, oid, name
+
+def get_tree(oid, base_path=''):
+    """
+    Recursively iterates through the entries of a Git tree object and 
+    returns a dictionary with the paths and object IDs of all blobs (files) in the tree.
+    """
+    result = {}
+    for type_, oid, name in _iter_tree_entries(oid):
+        assert '/' not in name
+        assert name not in ('..', '.')
+        path = base_path + name
+        if type_ == 'blob':
+            result[path] = oid
+        elif type_ == 'tree':
+            result.update(get_tree(oid, f'{path}/'))
+        else:
+            assert False, f'Unknown tree entry {type_}'
+    
+    return result
+
+def _empty_current_directory ():
+    """
+    To empty the current directory of all files and directories, excluding any files or directories specified in the ignore list.    reads a Git tree object and writes its contents to the current directory, 
+    overwriting any existing files or directories that are not ignored.
+    """
+    for root, dirnames, filenames in os.walk ('.', topdown=False):
+        for filename in filenames:
+            path = os.path.relpath (f'{root}/{filename}')
+            if is_ignored (path) or not os.path.isfile (path):
+                continue
+            os.remove (path)
+        for dirname in dirnames:
+            path = os.path.relpath (f'{root}/{dirname}')
+            if is_ignored (path):
+                continue
+            try:
+                os.rmdir (path)
+            except (FileNotFoundError, OSError):
+                # Deletion might fail if the directory contains ignored files,
+                # so it's OK
+                pass
+
+def read_tree (tree_oid):
+    """
+    reads a Git tree object and writes its contents to the current directory,
+    overwriting any existing files or directories that are not ignored.
+
+    Args: tree_oid(byte_string): The object id of the tree.
+    """
+    _empty_current_directory ()
+    for path, oid in get_tree (tree_oid, base_path='./').items ():
+        os.makedirs (os.path.dirname (path), exist_ok=True)
+        with open (path, 'wb') as f:
+            f.write (data.get_object (oid))
+
 def is_ignored(path):
-    return '.ugit' in Path(path).parts
+    return '.ugit' in path.split ('/')
--- ugit/cli.py
+++ ugit/cli.py
@@ -31,6 +31,9 @@ def parse_args():
     write_tree_parser = commands.add_parser('write-tree')
     write_tree_parser.set_defaults(func=write_tree)
     
+    read_tree_parser = commands.add_parser ('read-tree')
+    read_tree_parser.set_defaults (func=read_tree)
+    read_tree_parser.add_argument ('tree')
 
     return parser.parse_args()
 
@@ -40,13 +43,20 @@ def init(args):
     print('Initialized empy ugit repository in %s' 
     % os.path.join(os.getcwd(), data.GIT_DIR))
 
+
 def hash_object (args):
     with open (args.file, 'rb') as f:
         print (data.hash_object (f.read ()))
+
 
 def cat_file(args):
     sys.stdout.flush()
     sys.stdout.buffer.write(data.get_object(args.object, expected=None))
 
+
 def write_tree(args):
     print(base.write_tree())
+
+
+def read_tree(args):
+    base.read_tree(args.tree)
