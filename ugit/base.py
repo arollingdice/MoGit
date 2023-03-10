d0990d990936d12f6e69edf88927901a244b44ab
arollingdice <arollingdice@gmail.com>
2023-03-10 11:37:16 -0700

    implemented linked-list structure for commits so a commit can track a list of operations before or after it.

--- ugit/base.py
+++ ugit/base.py
@@ -93,10 +93,16 @@ def read_tree (tree_oid):
 
 def commit(msg):
     commit = f'tree {write_tree ()}\n'
+    HEAD = data.get_HEAD ()
+    if HEAD:
+        commit += f'parent {HEAD}\n'
     commit += '\n'
     commit += f'{msg}\n'
 
-    return data.hash_object(commit.encode(), 'commit')
+    oid = data.hash_object(commit.encode(), 'commit')
+    data.set_HEAD(oid)
+
+    return oid
 
 
 def is_ignored(path):
--- ugit/data.py
+++ ugit/data.py
@@ -10,14 +10,28 @@ def init():
     os.makedirs(GIT_DIR)
     os.makedirs(OBJECTS_DIR)
 
+
+def set_HEAD(oid):
+    with open(f'{GIT_DIR}/HEAD'. 'w') as f:
+        f.write(oid)
+
+
+def get_HEAD ():
+    if os.path.isfile (f'{GIT_DIR}/HEAD'):
+        with open (f'{GIT_DIR}/HEAD') as f:
+            return f.read ().strip ()
+
+
 def hash_object(data, type_='blob'):
-    obj = type_.encode() + NULL + data
+    obj = type_.encode() + NULL + dat:w
+    a
     oid = hashlib.sha1(obj).hexdigest()
 
     with open (OBJECTS_DIR / oid, 'wb') as out:
         out.write(obj)
 
     return oid
+
 
 def get_object(oid, expected='blob'):
     with open (OBJECTS_DIR / oid, 'rb') as f:
